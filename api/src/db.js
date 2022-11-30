4d7e165af09e17454b12db655259b5678c2fdfde
devxyzr <develop.xyzr@gmail.com>
2022-11-29 21:53:34 -0500

    Reload data

--- api/src/db.js
+++ api/src/db.js
@@ -5,10 +5,10 @@ const path = require('path');
 // const Country = require("./models/Country");
 // const Activity = require("./models/Activity");
 
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
